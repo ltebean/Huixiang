3e16840757bb7ab235babdf76393793663fe1f0b
ltebean <yucong1118@gmail.com>
2013-06-13 13:26:42 +0800

    Initial Commit

---
+++ Huixiang/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Huixiang
+//
+//  Created by ltebean on 13-6-13.
+//  Copyright (c) 2013年 ltebean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Huixiang/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Huixiang
+//
+//  Created by ltebean on 13-6-13.
+//  Copyright (c) 2013年 ltebean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
